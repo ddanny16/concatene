5bd767cdca4e5660e7a99328b878297c5b721594
ddanny16 <dammydan16@gmail.com>
2022-05-16 01:10:58 +0200

    console.log(trimmable.trim())

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ let z = "I am rich"
 let c = a.concat("",b,"",z)
 let numbers = [1,2,3,4,5,6,7]
 let imut ="ABCDEFGH"
+let trimmable = "   Ahmed Bolarinwa b ";
+let text = "5"
+let padded = text.padEnd(5,"x");
+let str4 = "Please locate where 'locate' occurs!";
+
 
 console.log(a + " " + b)
 console.log(c)
@@ -12,3 +17,8 @@ numbers[2] = 1000
 console.log(numbers[2])
 console.log(numbers)
 console.log(imut[4])
+imut[4] ='Z'
+console.log(imut[4])
+console.log(trimmable.trim())
+console.log(padded)
+console.log(str4.search("locate"));
