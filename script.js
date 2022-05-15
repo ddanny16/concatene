c3498b47012bb610fb1ad58e59abc038339f7ab1
ddanny16 <dammydan16@gmail.com>
2022-05-16 00:52:25 +0200

    console.log(numbers)

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ let z = "I am rich"
 let c = a.concat("",b,"",z)
 let numbers = [1,2,3,4,5,6,7]
 
-numbers[2] = 1000
+
 
  console.log(a + " " + b)
 console.log(c)
 console.log(numbers[2])
-console
+numbers[2] = 1000
+console.log(numbers[2])
+console.log(numbers)
