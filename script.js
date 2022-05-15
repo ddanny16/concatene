c4e8de05e2973045b3a9d1ec97cf398601a1406e
ddanny16 <dammydan16@gmail.com>
2022-05-16 00:54:46 +0200

    let imut =ABCDEFGH

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ let b = "welcome"
 let z = "I am rich"
 let c = a.concat("",b,"",z)
 let numbers = [1,2,3,4,5,6,7]
+let imut ="ABCDEFGH"
 
-
-
- console.log(a + " " + b)
+console.log(a + " " + b)
 console.log(c)
 console.log(numbers[2])
 numbers[2] = 1000
